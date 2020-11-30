bb1e34f2c46e0899a7d8bea3717cca24498957e6
Rafael Guillermo <rafael@garcia-irvine.com>
2020-11-30 17:28:21 +0100

    Update and rename countFilesInDriveFolder to countFilesInDriveFolder.js

--- countFilesInDriveFolder.js
+++ countFilesInDriveFolder.js
@@ -3,10 +3,10 @@
  * Returns number of files in a given Drive folder
  * Needs Advanced Drive Service to run
  * @param  {String} folderId A Drive folder ID
- * @return {Number} h number of files in given Drive folder
+ * @return {Number} count number of files in given Drive folder
  */
 function countFilesInFolder(folderId) {
-  var folder = Drive.Children.list(folderId)
+  var folder = Drive.Children.list(folderId);
   var count, tmp = folder.items.length;
   
   do {
