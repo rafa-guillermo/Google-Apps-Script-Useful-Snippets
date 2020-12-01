539ba48f34b413c192a766cf54c5ffda28dc8e32
Rafael Guillermo <rafael@garcia-irvine.com>
2020-12-01 11:51:27 +0100

    Update convertSheetToXlsx.js

--- sheets-snippets/convertSheetToXlsx.js
+++ sheets-snippets/convertSheetToXlsx.js
@@ -2,30 +2,37 @@
  * Converts Google Sheets file to Microsoft Excel Format and saves to Drive
  * Needs Advanced Drive Service to run
  * @param  {String} ssID - Spreadsheet ID for export
+ * @param  {String} folderId    - Folder ID of folder in which to save Sheet 
  * @return {Number}      - returns exit code 0 if successful, -1 if failed
  */
-function convertSheetToXlsx(ssID) {
-  var url = "https://docs.google.com/feeds/download/spreadsheets/Export?key=";
-  var exportMethod = "&exportFormat=xlsx";
-  var parameters = {
-    method: "get",
-    headers: {
-      "Authorization": "Bearer " + ScriptApp.getOAuthToken()
-    },
-    muteHttpExceptions: true
-  };
+function convertSheetToXlsx(ssID, folderId) {
+  try {
+    var url = "https://docs.google.com/feeds/download/spreadsheets/Export?key=";
+    var exportMethod = "&exportFormat=xlsx";
+    var parameters = {
+      method: "get",
+      headers: {
+        "Authorization": "Bearer " + ScriptApp.getOAuthToken()
+      },
+      muteHttpExceptions: true
+    };
 
-  var fileAsBlob = UrlFetchApp.fetch(url + ssID + exportMethod, parameters).getBlob();
+    var fileAsBlob = UrlFetchApp.fetch(url + ssID + exportMethod, parameters).getBlob();
 
-  fileAsBlob.setName(SpreadsheetApp.openById(ssID).getName() + ".xlsx");
+    fileAsBlob.setName(SpreadsheetApp.openById(ssID).getName() + ".xlsx");
 
-  var fileData = {
-    title: SpreadsheetApp.openById(ssID).getName() + ".xlsx",
-    mimeType: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-  };
+    var fileData = {
+      title: SpreadsheetApp.openById(ssID).getName() + ".xlsx",
+      mimeType: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+      parents: [
+        {
+          "kind": "drive#parentReference",
+          "id": folderId
+        }
+      ]
+    };
 
   // Save to Drive
-  try {
     Drive.Files.insert(fileData, fileAsBlob);
     return 0;
   }
