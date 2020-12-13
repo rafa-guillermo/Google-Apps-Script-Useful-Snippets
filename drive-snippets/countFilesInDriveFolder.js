1b298bb38bd5c25c690be64a5f9a7d33b758b269
Rafael Guillermo <rafael@garcia-irvine.com>
2020-12-13 02:10:12 +0100

    Update countFilesInDriveFolder.js

--- drive-snippets/countFilesInDriveFolder.js
+++ drive-snippets/countFilesInDriveFolder.js
@@ -5,12 +5,20 @@
  * @return {Number} count    - number of files in given Drive folder
  */
 function countFilesInFolder(folderId) {
+  
+  // Get list of items in a Drive folder
   var folder = Drive.Children.list(folderId);
+  
+  // Store current count
   var count = folder.items.length;
+  
+  // Store count in buffer variable
   var tmp = count;
   
   do {
+    // Check to see if there are more results not yet returned
     if (folder.nextPageToken) {
+      // Get the nect page of results and add to count
       folder = Drive.Children.list(folderId, {
         pageToken: folder.nextPageToken
       });
@@ -18,8 +26,10 @@ function countFilesInFolder(folderId) {
       count += tmp;
     }
   }
+  // Repeat as long as there are more results
   while (folder.nextPageToken);
   
+  // Log and return total count
   console.log(count);
   return count;
 }
